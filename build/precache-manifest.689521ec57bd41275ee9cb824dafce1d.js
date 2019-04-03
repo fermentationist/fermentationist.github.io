98de70a34f9759633006b9aff61a3ed539564854
Dennis Hodges <fermentationist@gmail.com>
2019-04-03 03:29:09 -0500

    Overwriting master with build from gh-pages

---
+++ build/precache-manifest.689521ec57bd41275ee9cb824dafce1d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "01430b01ebefe6f20a8529dd4b5de79e",
+    "url": "/static/media/graph-paper.01430b01.svg"
+  },
+  {
+    "revision": "44d640c897cc9be76a56",
+    "url": "/static/css/main.1401c503.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c5185d07ae795b0180f6",
+    "url": "/static/js/2.0531cd67.chunk.js"
+  },
+  {
+    "revision": "ee883d3a32bb05f856d5f39631aa626f",
+    "url": "/static/media/hideout.ee883d3a.svg"
+  },
+  {
+    "revision": "d41813002e74de7792cc0dcebae9ae2d",
+    "url": "/static/media/skin_side_up_transparent.d4181300.png"
+  },
+  {
+    "revision": "00b22e9ea40e3b3fb5a02ea48f4e7961",
+    "url": "/static/media/seigaiha.00b22e9e.png"
+  },
+  {
+    "revision": "44d640c897cc9be76a56",
+    "url": "/static/js/main.de67f73b.chunk.js"
+  },
+  {
+    "revision": "47f8a6f9be318a490f66e8cbf6b6c178",
+    "url": "/static/media/perforated_white_leather_@2X.47f8a6f9.png"
+  },
+  {
+    "revision": "7870a283a376489ff1f12c8e9b2a26a7",
+    "url": "/static/media/white_leather_@2X.7870a283.png"
+  },
+  {
+    "revision": "3ce3b93b031c5cd29bdec04abf618352",
+    "url": "/static/media/pixel-dennis-dim-long.3ce3b93b.gif"
+  },
+  {
+    "revision": "25ded716438ef89a8bd12bc31108e583",
+    "url": "/static/media/pixel-dennis-dim-long-eyebrow.25ded716.gif"
+  },
+  {
+    "revision": "be0572fc07814e0418ea7b2875417af7",
+    "url": "/static/media/pixel-dennis-long.be0572fc.gif"
+  },
+  {
+    "revision": "ce1156707e41cb9ed68ef62fafbf7c79",
+    "url": "/static/media/pixel-dennis-long-eyebrow.ce115670.gif"
+  },
+  {
+    "revision": "59b8df7f64e31dd8ea91b791a2f508c9",
+    "url": "/index.html"
+  }
+];
