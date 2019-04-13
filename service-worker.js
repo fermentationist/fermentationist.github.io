3a6e600a7c7a22d6906d46ebb47d37b53396b535
Dennis Hodges <fermentationist@gmail.com>
2019-04-13 00:41:42 -0500

    Site update copied from dennis-hodges-dot-com, gh-pages branch. Ready to deploy to fermentationist.github.io.

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dennis-hodges-dot-com/precache-manifest.ceb42222dd56b0d4199b1a36d5d17308.js"
+  "/precache-manifest.8753e27ddf151e886355f8bfeb766e22.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/dennis-hodges-dot-com/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
