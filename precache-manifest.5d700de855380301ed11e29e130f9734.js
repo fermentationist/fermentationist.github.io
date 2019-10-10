be0f1e18e6d3dffd7d8984df5729b560f3a8214c
Dennis Hodges <fermentationist@gmail.com>
2019-10-10 14:10:01 -0500

    new build of dennis-hodges-dot-com with webp removed

---
+++ precache-manifest.5d700de855380301ed11e29e130f9734.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b1b591a8b694e5aa407",
+    "url": "/static/css/main.ee46faf7.chunk.css"
+  },
+  {
+    "revision": "2b1b591a8b694e5aa407",
+    "url": "/static/js/main.9184afc8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1258280f74b6496d6ca3",
+    "url": "/static/js/2.0dcf8482.chunk.js"
+  },
+  {
+    "revision": "00b22e9ea40e3b3fb5a02ea48f4e7961",
+    "url": "/static/media/seigaiha.00b22e9e.png"
+  },
+  {
+    "revision": "45edef9658b239b7022c9acf4177d02d",
+    "url": "/static/media/urnwood_dark_large.45edef96.jpg"
+  },
+  {
+    "revision": "4872041da10dd7cb59b964cc8f0feda2",
+    "url": "/static/media/urnwood_light_large.4872041d.jpg"
+  },
+  {
+    "revision": "da77ecc7d8e5239a715504bca407686d",
+    "url": "/static/media/urnwood_dark_cropped_small.da77ecc7.jpg"
+  },
+  {
+    "revision": "c6a2e278c2772dc0791dbf4d0dd38b50",
+    "url": "/static/media/urnwood_light_cropped_small.c6a2e278.jpg"
+  },
+  {
+    "revision": "68e69ed79656b0938b51a72974565ef6",
+    "url": "/static/media/gray_kraft.68e69ed7.webp"
+  },
+  {
+    "revision": "3ce3b93b031c5cd29bdec04abf618352",
+    "url": "/static/media/pixel-dennis-dim-long.3ce3b93b.gif"
+  },
+  {
+    "revision": "25ded716438ef89a8bd12bc31108e583",
+    "url": "/static/media/pixel-dennis-dim-long-eyebrow.25ded716.gif"
+  },
+  {
+    "revision": "be0572fc07814e0418ea7b2875417af7",
+    "url": "/static/media/pixel-dennis-long.be0572fc.gif"
+  },
+  {
+    "revision": "ce1156707e41cb9ed68ef62fafbf7c79",
+    "url": "/static/media/pixel-dennis-long-eyebrow.ce115670.gif"
+  },
+  {
+    "revision": "53cfc13e71e5b446aad996c8aa9007a1",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bbf0747298eb0ca23d7d694c176d51a6.js"
+  "/precache-manifest.5d700de855380301ed11e29e130f9734.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
